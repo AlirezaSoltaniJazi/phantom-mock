a8dc1843092e9ed3263eac93681e5004dfe15205
Alireza Soltani <alireza.soltani.jazi@gmail.com>
2026-05-21 23:12:15 +0100

    Add JSX type import to devtools panel component

--- src/devtools/panel.tsx
+++ src/devtools/panel.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, type JSX } from 'react';
 import { createRoot } from 'react-dom/client';
 import { useAppState } from './state-hook';
 import { RulesTable } from './components/RulesTable';
